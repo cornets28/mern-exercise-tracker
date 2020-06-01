71b953cb233145b41c463a0df0ab5022500813bd
CornetS28 <corsam28@gmail.com>
2020-06-01 14:21:57 -0400

    create the very basic components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Navbar from './components/nabar.component';
-import ExercisesList from './components/exercises-list.component';
-import EditExercise from './components/edit-exercise.component';
-import CreateExercise from './components/create-exercise.component';
-import CreateUser from './components/create-user.component'
-
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
@@ -15,9 +14,9 @@ function App() {
       <Navbar />
       <br />
       <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" exact component={EditExercise} />
-      <Route path="/create" exact component={CreateExercise} />
-      <Route path="/user" exact component={CreateUser} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
     </Router>
   );
 }
---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CreateExercices extends Component {
+  render() {
+    return (
+      <div>
+        <p className="">This Create exercises</p>
+      </div>
+    );
+  }
+}
