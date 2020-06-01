022a2dfed3b404e667ea5114ece59d94d4aa520f
CornetS28 <corsam28@gmail.com>
2020-06-01 00:42:03 -0400

    create models directory

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established succesfully");
+});
+
 app.listen(port, () => {
   console.log(`Server is now running on port: ${port}`);
 })
