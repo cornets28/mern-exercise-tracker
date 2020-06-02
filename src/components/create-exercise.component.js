a62c125ba20ec06bc42a1936bb1d9c8bebad4e7f
CornetS28 <corsam28@gmail.com>
2020-06-02 11:11:45 -0400

    fix exercise's link

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -65,7 +65,7 @@ export default class CreateExercices extends Component {
     
      console.log(exercise);
      axios
-       .post("http://localhost:5000/exercise/add", exercise)
+       .post("http://localhost:5000/exercises/add", exercise)
        .then((res) => console.log(res.data));
     window.location = "/";
    
