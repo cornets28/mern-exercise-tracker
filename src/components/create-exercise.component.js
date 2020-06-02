b8d7251582d5bbbfebd92b351b33f4f3c691daad
CornetS28 <corsam28@gmail.com>
2020-06-01 22:28:23 -0400

    create methods for the input form

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,6 +1,52 @@
+/* eslint-disable no-useless-constructor */
 import React, { Component } from "react";
 
 export default class CreateExercices extends Component {
+  constructor(props) {
+    super(props);
+
+    this.usernameOnChange = this.usernameOnChange.bind(this);
+    this.descriptionOnchange = this.descriptionOnchange.bind(this);
+    this.durationtionOnChange = this.durationtionOnChange.bind(this);
+    this.dateOnChange = this.dateOnChange.bind(this);
+
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: []
+    }
+  }
+  usernameOnChange(event) {
+    this.setState({ username: event.target.value })
+  }
+
+   descriptionOnchange(event) {
+    this.setState({ description: event.target.value })
+  }
+
+   durationtionOnChange(event) {
+    this.setState({ duration: event.target.value })
+  }
+
+  dateOnChange(date) {
+    this.setState({ date })
+  }
+
+  onSubmit() {
+    event.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
+    };
+    console.log(exercise);
+    window.location = "/";
+  }
+
   render() {
     return (
       <div>
